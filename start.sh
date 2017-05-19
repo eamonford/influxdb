22c0ac830ad6c7a67d03317e6a0670b09bcc608a
Eamon Ford <eamonford@gmail.com>
2017-05-18 23:28:56 -0500

    feat(retention-policy): create retention policy

--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@ influxd&
 sleep 5
 if [ $DB_NAME ]; then
     influx --execute "CREATE DATABASE ${DB_NAME}";
+    influx --execute "CREATE RETENTION POLICY default_policy ON ${DB_NAME} DURATION ${DAYS_TO_RETAIN}d REPLICATION 1 DEFAULT";
 fi
 tail -f /var/log/dmesg
